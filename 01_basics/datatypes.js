d359e9696b2fc8bc182f635c74f4e94064171403
zuhairsaleem10 <saleemzuhair10@gmail.com>
2026-05-21 13:07:15 +0000

    container config file

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+let name = "zuhair"
+// string
+let age = 21;
+// number
+let isAvailable = true;
+// boolean
+let city;
+// undefined
+let salary = null;
+// null
+let largeNumber = BigInt("1234567890123456789012345678901234567890");
+// BigInt
+let uniqueID= Symbol("f2023065298");
+// Symbol
+// how to know datatype of variable? use console.log(typeof variableName);
+console.table([name, age, isAvailable, city, salary, largeNumber, uniqueID]);
+
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,4 +1,6 @@
 const accountID = "f2023065298";
 let accountBalance =10000;
 var accountType = "Salary";
+city=Bhakkar;
 console.table([accountID, accountName, accountBalance, accountType]);
+// prefer not to use var because of its function scope and hoisting issues. Instead, use let and const for better readability and maintainability of code.
